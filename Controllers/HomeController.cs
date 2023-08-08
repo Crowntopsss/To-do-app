7991661da93578b2ec55dfae039bb13063e53e0f
Temitope Adebiyi <adebiyitope26@gmail.com>
2023-08-09 00:31:12 +0100

    Added comments to Home controller

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -8,16 +8,19 @@ namespace To_do_app.Controllers
     {
         private static List<ToDo> toDoList = new List<ToDo>();
 
+        // Action to display the To-Do list
         public IActionResult Index()
         {
             return View(toDoList);
         }
 
+        // Action to add a new To-Do
         [HttpPost]
         public IActionResult Add(string description)
         {
             if (!string.IsNullOrWhiteSpace(description))
             {
+                // Create a new To-Do item
                 var newToDo = new ToDo
                 {
                     Id = Guid.NewGuid(),
@@ -27,18 +30,22 @@ namespace To_do_app.Controllers
                     CreationDate = DateTime.Now 
                 };
 
+                // Add the new To-Do item to the list
                 toDoList.Add(newToDo);
             }
 
             return RedirectToAction("Index");
         }
 
+        // Action to mark a To-Do as done
         [HttpPost]
         public IActionResult MarkAsDone(Guid id)
         {
+            // Find the To-Do item by its ID
             var todo = toDoList.Find(t => t.Id == id);
             if (todo != null)
             {
+                // Mark the To-Do as done and record completion date
                 todo.IsDone = true;
                 todo.CompletionDate = DateTime.Now;
             }
